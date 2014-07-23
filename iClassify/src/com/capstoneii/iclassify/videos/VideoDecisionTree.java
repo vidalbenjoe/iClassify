95a0e487d42712df2e991da982b34059258dfb2f
vidalbenjoe <benjoe22@gmail.com>
2014-07-24 01:07:24 +0800

    Updated Video

--- iClassify/src/com/capstoneii/iclassify/videos/VideoDecisionTree.java
+++
@@ -1,5 +0,0 @@
-package com.capstoneii.iclassify.videos;
-
-public class VideoDecisionTree {
-
-}
--- iClassify/src/com/capstoneii/iclassify/videos/VideoMenuActivity.java
+++ iClassify/src/com/capstoneii/iclassify/videos/VideoMenuActivity.java
@@ -14,18 +14,22 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 
 public class VideoMenuActivity extends ActionBarActivity {
 	private VideoView vv;
-	public String videoFile1;
-	public String videoFile2;
+	public String videoFile1, videoFile2,videoFile3;
+	public TextView setvideoDesc;
+	
+	
 
     private ExpandableMenuOverlay menuOverlay;
 
@@ -37,10 +41,18 @@ public class VideoMenuActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         
         vv = (VideoView) findViewById(R.id.videoView1);
-		videoFile1 = "android.resource://" + getPackageName() + "/" + R.raw.videoidtree;
+		videoFile1 = "android.resource://" + getPackageName() + "/" + R.raw.videoknn;
 		
-		videoFile2 = "android.resource://" + getPackageName() + "/" + R.raw.videoknn;
-		    
+		videoFile2 = "android.resource://" + getPackageName() + "/" + R.raw.videoidtree;
+		 
+		videoFile3 = "android.resource://" + getPackageName() + "/" + R.raw.videonaive;
+		 
+		
+		
+		setvideoDesc = (TextView) findViewById(R.id.videoDesc);
+		setvideoDesc.setMovementMethod(new ScrollingMovementMethod());
+		setvideoDesc.setText(R.string.knnvideodesc);
+		
 		MediaController mc = new MediaController(this);
 	    mc.setAnchorView(vv);
         mc.setMediaPlayer(vv);
@@ -49,43 +61,57 @@ public class VideoMenuActivity extends ActionBarActivity {
         vv.setVideoURI(uri);
         vv.requestFocus();
         vv.start();
-		
         
+		
         menuOverlay = (ExpandableMenuOverlay) findViewById(R.id.button_menu);
 
         menuOverlay.setOnMenuButtonClickListener(new ExpandableButtonMenu.OnMenuButtonClick() {
             @Override
             public void onClick(ExpandableButtonMenu.MenuButton action) {
                 switch (action) {
-                    case MID:
-                       
+                    case MID://knn
                         menuOverlay.getButtonMenu().toggle();
-                      
                         MediaController mc1 = new MediaController(VideoMenuActivity.this);
                 	    mc1.setAnchorView(vv);
                         mc1.setMediaPlayer(vv);
-                        Uri uri = Uri.parse(videoFile2);
+                        Uri uri = Uri.parse(videoFile1);
                         vv.setMediaController(mc1);
                         vv.setVideoURI(uri);
                         vv.requestFocus();
                         vv.start();
+                        setvideoDesc = (TextView) findViewById(R.id.videoDesc);
+                        setvideoDesc.setMovementMethod(new ScrollingMovementMethod());
+                        setvideoDesc.setText(R.string.knnvideodesc);
                         break;
                         
-                    case LEFT:
+                    case LEFT://idr
                     	menuOverlay.getButtonMenu().toggle();
                     	MediaController mc2 = new MediaController(VideoMenuActivity.this);
                 	    mc2.setAnchorView(vv);
                         mc2.setMediaPlayer(vv);
-                        Uri uri2 = Uri.parse(videoFile1);
+                        Uri uri2 = Uri.parse(videoFile2);
                         vv.setMediaController(mc2);
                         vv.setVideoURI(uri2);
                         vv.requestFocus();
                         vv.start();
-                    	
+                        setvideoDesc = (TextView) findViewById(R.id.videoDesc);
+                        setvideoDesc.setMovementMethod(new ScrollingMovementMethod());
+                        setvideoDesc.setText(R.string.treevideodesc);
                     	
                         break;
-                    case RIGHT:
-                        Toast.makeText(VideoMenuActivity.this, "Native Bayes", Toast.LENGTH_SHORT).show();
+                    case RIGHT://naive
+                    	menuOverlay.getButtonMenu().toggle();
+                    	MediaController mc3 = new MediaController(VideoMenuActivity.this);
+                	    mc3.setAnchorView(vv);
+                        mc3.setMediaPlayer(vv);
+                        Uri uri3 = Uri.parse(videoFile3);
+                        vv.setMediaController(mc3);
+                        vv.setVideoURI(uri3);
+                        vv.requestFocus();
+                        vv.start();
+                        setvideoDesc = (TextView) findViewById(R.id.videoDesc);
+                        setvideoDesc.setMovementMethod(new ScrollingMovementMethod());
+                        setvideoDesc.setText(R.string.naivevideodesc);
                         break;
                 }
             }
